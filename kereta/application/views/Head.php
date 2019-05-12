50cb859474547407ac105695f631e491b22cb40a
rzplv <48095133+rzplv@users.noreply.github.com>
2019-05-12 21:23:27 +0700

    Update Head.php

--- kereta/application/views/Head.php
+++ kereta/application/views/Head.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
   <title>PRA6 PROMNET</title>
+  <title>PRA7 PROMNET (github)</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="<?php echo base_url(); ?>/css/bootstrap.min.css">
@@ -11,6 +12,7 @@
 
   <nav class="navbar navbar-expand-lg navbar-dark bg-success">
     <a class="navbar-brand" href="#">KERETA JOIN PRA6</a>
+    <a class="navbar-brand" href="#">KERETA JOIN PRA7</a>
     <button class="navbar-toggler" aria-expanded="false" aria-controls="navbarColor01" aria-label="Toggle navigation" type="button" data-target="#navbarColor01" data-toggle="collapse">
       <span class="navbar-toggler-icon"></span>
     </button>
